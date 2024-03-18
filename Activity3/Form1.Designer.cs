550d19b417b77df099db6aef83a24c2c59664a43
kyle.canonigoa@cit.edu <kyle.canonigoa@cit.edu>
2024-03-18 14:03:03 +0800

    Add project files.

---
+++ Activity3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Activity3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblMouse = new System.Windows.Forms.Label();
+            this.lblSquare = new System.Windows.Forms.Label();
+            this.lblSelected = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 321);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // lblMouse
+            // 
+            this.lblMouse.AutoSize = true;
+            this.lblMouse.Location = new System.Drawing.Point(434, 69);
+            this.lblMouse.Name = "lblMouse";
+            this.lblMouse.Size = new System.Drawing.Size(35, 13);
+            this.lblMouse.TabIndex = 1;
+            this.lblMouse.Text = "label1";
+            // 
+            // lblSquare
+            // 
+            this.lblSquare.AutoSize = true;
+            this.lblSquare.Location = new System.Drawing.Point(434, 100);
+            this.lblSquare.Name = "lblSquare";
+            this.lblSquare.Size = new System.Drawing.Size(35, 13);
+            this.lblSquare.TabIndex = 2;
+            this.lblSquare.Text = "label1";
+            // 
+            // lblSelected
+            // 
+            this.lblSelected.AutoSize = true;
+            this.lblSelected.Location = new System.Drawing.Point(434, 127);
+            this.lblSelected.Name = "lblSelected";
+            this.lblSelected.Size = new System.Drawing.Size(35, 13);
+            this.lblSelected.TabIndex = 3;
+            this.lblSelected.Text = "label1";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(437, 168);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(35, 13);
+            this.lblPath.TabIndex = 4;
+            this.lblPath.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(426, 206);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(126, 108);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 343);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblSelected);
+            this.Controls.Add(this.lblSquare);
+            this.Controls.Add(this.lblMouse);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblMouse;
+        private System.Windows.Forms.Label lblSquare;
+        private System.Windows.Forms.Label lblSelected;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Activity3/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+// Kyle A. Canonigo | BSCS 3 F1 | ACT 3 | BFS
+namespace Activity3
+{
+    public partial class Form1 : Form
+    {
+        int side;
+        int numX = 20;
+        int numY = 16;
+        Square[,] grid;
+
+        Square highlighted = new Square();
+        Square selected = new Square(0, 0);
+
+        //int mode = 0;
+        LinkedList<PathNode> tree;
+        Queue<PathNode> search;
+        int exploreLimit;
+        int exploreCount;
+
+        PathNode goalNode;
+
+        bool changedSquare = false;
+        int currentX, currentY;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            grid = new Square[numX, numY];
+            exploreLimit = numX * numY;
+            resetGrid();
+            side = Convert.ToInt16(pictureBox1.Width / numX);
+
+            tree = new LinkedList<PathNode>();
+            tree.AddFirst(new PathNode(selected));
+            search = new Queue<PathNode>();
+            search.Enqueue(tree.First.Value);
+        }
+
+        public void resetGrid()
+        {
+            for (int x = 0; x < numX; x++)
+            {
+                for (int y = 0; y < numY; y++)
+                {
+                    grid[x, y] = new Square(x, y);
+                }
+            }
+        }
+
+        public bool isValid(int x, int y)
+        {
+            if (x >= 0 && x < numX && y >= 0 && y < numY)
+                return true;
+            else
+                return false;
+        }
+
+        private bool hasArrived(PathNode target)
+        {
+            return target.location.X == highlighted.X && target.location.Y == highlighted.Y;
+        }
+
+        private void exploreNode(PathNode target)
+        {
+            // UP DOWN LEFT RIGHT
+            Point UP = new Point(target.location.X, target.location.Y - 1);
+            Point DOWN = new Point(target.location.X, target.location.Y + 1);
+            Point LEFT = new Point(target.location.X - 1, target.location.Y);
+            Point RIGHT = new Point(target.location.X + 1, target.location.Y);
+
+            Point[] direction = { UP, DOWN, LEFT, RIGHT };
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (isValid(direction[i].X, direction[i].Y))
+                {
+                    Square check = grid[direction[i].X, direction[i].Y];
+                    if (check.passable && !check.explored)
+                    {
+                        tree.AddLast(new PathNode(grid[direction[i].X, direction[i].Y], target));
+                        search.Enqueue(tree.Last.Value);
+                        grid[direction[i].X, direction[i].Y].explored = true;
+                    }
+                }
+            }
+            exploreCount++;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            highlighted.X = e.X / side;
+            highlighted.Y = e.Y / side;
+
+            tree = new LinkedList<PathNode>();
+            tree.AddFirst(new PathNode(selected));
+            search = new Queue<PathNode>();
+            search.Enqueue(tree.First.Value);
+            bool foundGoal = false;
+
+            if (!changedSquare)
+            {
+                currentX = highlighted.X;
+                currentY = highlighted.Y;
+                changedSquare = true;
+
+                while (foundGoal == false && search.Count > 0)
+                {
+                    PathNode target = (PathNode)search.Dequeue();
+                    if (target != null)
+                    {
+                        foundGoal = hasArrived(target);
+                        if (foundGoal)
+                        {
+                            foundGoal = true;
+                            goalNode = target;
+                            lblPath.Text = "PathFound";
+                            tree.Clear();
+                            tree.AddFirst(new PathNode(selected));
+                            search.Clear();
+                            search.Enqueue(tree.First.Value);
+                            resetGrid();
+                        }
+                        else
+                        {
+                            exploreNode(target);
+                            //listBox1.Items = search;
+                        }
+                    }
+                }
+                pictureBox1.Refresh();
+            }
+
+            if (currentX != highlighted.X || currentY != highlighted.Y)
+            {
+                changedSquare = false;
+                listBox1.Items.Clear();
+            }
+
+            lblMouse.Text = "X: " + e.X + " Y: " + e.Y;
+            lblSquare.Text = "( " + highlighted.X + " , " + highlighted.Y + " ) ";
+        }
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int x = 0; x < numX; x++)
+            {
+                for (int y = 0; y < numY; y++)
+                {
+                    Rectangle s = new Rectangle(x * side, y * side, side, side);
+                    if (x == selected.X && y == selected.Y)
+                        e.Graphics.FillRectangle(Brushes.SlateBlue, s);
+                    else if (x == highlighted.X && y == highlighted.Y)
+                        e.Graphics.FillRectangle(Brushes.SkyBlue, s);
+                    else
+                        e.Graphics.FillRectangle(Brushes.Silver, s);
+                    e.Graphics.DrawRectangle(Pens.Gray, s);
+                }
+            }
+
+            while (goalNode != null)
+            {
+                if (goalNode.location.X != selected.X || goalNode.location.Y != selected.Y)
+                {
+                    Rectangle s = new Rectangle(goalNode.location.X * side,
+                                            goalNode.location.Y * side,
+                                            side, side);
+                    e.Graphics.FillRectangle(Brushes.DarkBlue, s);
+                }
+
+                listBox1.Items.Add("("+goalNode.location.X.ToString() + "," + goalNode.location.Y.ToString()+")");
+
+                goalNode = goalNode.origin;
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            selected.X = e.X / side;
+            selected.Y = e.Y / side;
+
+            //pictureBox1.Refresh();
+
+            lblSelected.Text = "( " + selected.X + " , " + selected.Y + " ) ";
+            listBox1.Items.Clear();
+        }
+    }
+}
